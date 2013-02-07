66f8951169d18d58c2048f867ffcdf0a3a633bc0
Jan-Willem Gmelig Meyling <jan-willem@meylingmedia.nl>
2013-02-07 16:13:25 +0100

    Calendar can now be placed inside content

--- jw-jqm-cal.js
+++ jw-jqm-cal.js
@@ -2,30 +2,37 @@
    $.jqmCalendar = function(element, options) {
       
       var defaults = {
+		 // Array of events
          events : [],
+		 // Date variable to determine which month to show and which date to select
          date : new Date(),
+		 // Array of month strings (calendar header)
          months : ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+		 // Array of day strings (calendar header)
          days : ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+		 // Most months contain 5 weeks, some 6. Set this to six if you don't want the amount of rows to change when switching months.
          weeksInMonth : undefined,
+		 // [TODO]
          hideWeekends : false,
+		 // Start the week at the day of your preference, 0 for sunday, 1 for monday, and so on.
          startOfWeek : 0,   
       }
 
       var plugin = this;
       plugin.settings = null;
 
-      var $element = $(element),
+      var $element = $(element).addClass("jq-calendar-wrapper"),
           element = element,
-         $table,
-         $header,
-         $tbody,
-         $listview;
+          $table,
+          $header,
+          $tbody,
+          $listview;
 
       function init() {
          plugin.settings = $.extend({}, defaults, options);
+         $table = $("<table/>");
          
-         $table = $("<table class='jw-jqm-table'/>");
-         
+		 // Build the header
          var $thead = $("<thead/>").appendTo($table),
             $tr = $("<tr/>").appendTo($thead),
             $th = $("<th class='ui-bar-c header' colspan='7'/>");
@@ -44,6 +51,7 @@
          
          $tr = $("<tr/>").appendTo($thead);
          
+		 // The way of determing the labels for the days is a bit awkward, but works.
          for ( var i = 0, days = [].concat(plugin.settings.days, plugin.settings.days).splice(plugin.settings.startOfWeek, 7); i < 7; i++ ) {
             $tr.append("<th class='ui-bar-c'><span class='hidden'>"  + days[i] + "</span></th>");
          }
@@ -52,6 +60,8 @@
          
          $table.appendTo($element);
          $listview = $("<ul data-role='listview'/>").insertAfter($table);
+		 
+		 // Call refresh to fill the calendar with dates
          refresh(plugin.settings.date);      
       }
       
@@ -112,8 +122,10 @@
          $this.addClass("ui-btn-active");
          
          if ( date.getMonth() !== plugin.settings.date.getMonth() ) {
+		    // Go to previous/next month
             refresh(date);
          } else {
+            // Select new date
             $element.trigger('change', date);
          }
       }
@@ -123,12 +135,13 @@
                   
          var year = date.getFullYear(),
             month = date.getMonth(),
-            firstDayOfMonth = _firstDayOfMonth(date),
-            daysBefore = _daysBefore(date, firstDayOfMonth),
+            daysBefore = _daysBefore(date),
             daysInMonth = _daysInMonth(date),
             weeksInMonth = _weeksInMonth(date, daysInMonth, daysBefore);
          
-         $tbody.empty(); 
+		 // Empty the table body, we start all over...
+         $tbody.empty();
+		 // Change the header to match the current month
          $header.html( plugin.settings.months[month] + " " + year.toString() );
       
          for (    var   weekIndex = 0,
@@ -137,19 +150,22 @@
                      
             var daysInWeekCount = 0,
                row = $("<tr/>").appendTo($tbody);
-                        
+            
+		    // Previous month
             while ( daysBefore > 0 ) {
                addCell(row, new Date(year, month, 1 - daysBefore), true);
                daysBefore--;
                daysInWeekCount++;
             }
             
+		    // Current month
             while ( daysInWeekCount < 7 && daysInMonthCount <= daysInMonth ) {
                addCell(row, new Date(year, month, daysInMonthCount), false, daysInMonthCount === date.getDate() );
                daysInWeekCount++;
                daysInMonthCount++;
             }
             
+		    // Next month
             while ( daysInMonthCount > daysInMonth && daysInWeekCount < 7 ) {
                addCell(row, new Date(year, month, daysInMonth + daysAfterCount), true);
                daysInWeekCount++;
@@ -161,11 +177,14 @@
       }
 
       $element.bind('change', function(event, begin) {
-         var   end = new Date(begin.getFullYear(), begin.getMonth(), begin.getDate() + 1, 0,0,0,0);
+         var end = new Date(begin.getFullYear(), begin.getMonth(), begin.getDate() + 1, 0,0,0,0);
+         // Empty the list
          $listview.empty();
 
-         for (    var   i = 0, event; event = plugin.settings.events[i]; i++ ) {
+         // Find events for this date
+         for ( var   i = 0, event; event = plugin.settings.events[i]; i++ ) {
             if ( event.end > begin && event.begin < end ) {
+               // Append matches to list
                var summary    = event["summary"],
                   beginTime   = event["begin"].toTimeString().substr(0,5),
                   endTime      = event["end"].toTimeString().substr(0,5),
